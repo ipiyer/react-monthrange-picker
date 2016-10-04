a28b016d74b5106e51a2786dc041dfa26ea4b3a9
Prashanth Iyer <pgopi@yahoo-inc.com>
2016-10-04 11:07:19 -0700

    Adding karma.

---
+++ test/components/date_change.js
@@ -0,0 +1,9 @@
+"use strict";
+var chai = require('chai'),
+  expect = chai.expect;
+
+describe("example test", function() {
+  it("should be true", function() {
+    expect(true).to.be.true;
+  })
+})
